cbc2039d0ca4a025a3ca3c8b3337a5ca5b02d6b9
Ben Roberts <ben@benroberts.me>
2025-12-16 10:11:44 -0800

    DIN and phyto photic zone extraction

--- din.sh
+++ din.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-# FIXME I'm not sure I understand the purpose of this data and what we're looking for, which is important for deciding how to reduce it. ie, should I be extracting all, or just some DIN data (photic)?
 
 set -e
+
+outfile=results/din.nc
 
 . "$HOME/mambaforge/etc/profile.d/conda.sh"
 . "$HOME/mambaforge/etc/profile.d/mamba.sh"
 conda activate ssm-analysis
 export PATH=$HOME/src/ssm-analysis:$PATH
 
-if ! ncdump -v controlNO32 din.nc &> /dev/null; then
-	rawcdf_extract.py -v --invar NH4:all --invar NO32:all -s AdmInlet1 SwinomishChannelSouth DeceptionPassEast -- control/wqm/outputs/ssm_FVCOMICM_*.nc din.nc control
+if ! ncdump -v controlNO3_mean,photic $outfile &> /dev/null; then
+	rawcdf_extract.py -v --invar NH4:photic,mean --invar NO3:photic,mean -s AdmInlet1 SwinomishChannelSouth DeceptionPassEast -- control/wqm/outputs/ssm_FVCOMICM_*.nc $outfile control
 fi
-if ! ncdump -v c2936NO32 din.nc &> /dev/null; then
-	rawcdf_extract.py -v --invar NH4:all --invar NO32:all control/wqm_u29.36/outputs/ssm_FVCOMICM_*.nc din.nc c2936
+if ! ncdump -v c2936NO3_mean,photic $outfile &> /dev/null; then
+	rawcdf_extract.py -v --invar NH4:photic,mean --invar NO3:photic,mean control/wqm_u29.36/outputs/ssm_FVCOMICM_*.nc $outfile c2936
 fi
 
 for q in 1 3 5 7 9; do
@@ -24,11 +25,11 @@ for q in 1 3 5 7 9; do
 			continue
 		fi
 		var=q${q}c${c/./}
-		if ncdump -v ${var}NO32 din.nc &> /dev/null; then
+		if ncdump -v ${var}NO3_mean,photic $outfile &> /dev/null; then
 			echo ' (skipped, already extracted)'
 			continue
 		fi
 		echo
-		rawcdf_extract.py -v --invar NH4:all --invar NO32:all q0.$q/wqm_$c/outputs/ssm_FVCOMICM_*.nc din.nc $var
+		rawcdf_extract.py -v --invar NH4:photic,mean --invar NO3:photic,mean q0.$q/wqm_$c/outputs/ssm_FVCOMICM_*.nc $outfile $var
 	done
 done
--- phyto.sh
+++ phyto.sh
@@ -2,16 +2,18 @@
 
 set -e
 
+outfile=results/phyto.nc
+
 . "$HOME/mambaforge/etc/profile.d/conda.sh"
 . "$HOME/mambaforge/etc/profile.d/mamba.sh"
 conda activate ssm-analysis
 export PATH=$HOME/src/ssm-analysis:$PATH
 
-if ! ncdump -v controlB1 phyto.nc &> /dev/null; then
-	rawcdf_extract.py -v --invar B1:phyto --invar B2:phyto -s AdmInlet1 SwinomishChannelSouth DeceptionPassEast -- control/wqm/outputs/ssm_FVCOMICM_*.nc phyto.nc control
+if ! ncdump -v controlB1_mean,photic $outfile &> /dev/null; then
+	rawcdf_extract.py -v --invar B1:photic,mean --invar B2:photic,mean -s AdmInlet1 SwinomishChannelSouth DeceptionPassEast -- control/wqm/outputs/ssm_FVCOMICM_*.nc $outfile control
 fi
-if ! ncdump -v c2936B1 phyto.nc &> /dev/null; then
-	rawcdf_extract.py -v --invar B1:phyto --invar B2:phyto control/wqm_u29.36/outputs/ssm_FVCOMICM_*.nc phyto.nc c2936
+if ! ncdump -v c2936B1_mean,photic $outfile &> /dev/null; then
+	rawcdf_extract.py -v --invar B1:photic,mean --invar B2:photic,mean control/wqm_u29.36/outputs/ssm_FVCOMICM_*.nc $outfile c2936
 fi
 
 for q in 1 3 5 7 9; do
@@ -23,11 +25,11 @@ for q in 1 3 5 7 9; do
 			continue
 		fi
 		var=q${q}c${c/./}
-		if ncdump -v ${var}B1 phyto.nc &> /dev/null; then
+		if ncdump -v ${var}B1_mean,photic $outfile &> /dev/null; then
 			echo ' (skipped, already extracted)'
 			continue
 		fi
 		echo
-		rawcdf_extract.py -v --invar B1:phyto --invar B2:phyto q0.$q/wqm_$c/outputs/ssm_FVCOMICM_*.nc phyto.nc $var
+		rawcdf_extract.py -v --invar B1:photic,mean --invar B2:photic,mean q0.$q/wqm_$c/outputs/ssm_FVCOMICM_*.nc $outfile $var
 	done
 done
